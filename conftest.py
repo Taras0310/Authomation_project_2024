ae68677e273bb2b12fe56e963ced53ffacf82dd8
Taras0310 <taras03101999@ukr.net>
2024-11-02 22:24:16 +0100

    Added some http and api tests

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from modules.api.clients.github import Github
 
 class User:
 
@@ -23,3 +24,10 @@ def user():
     yield user
 
     user.remove()
+
+
+@pytest.fixture()
+def github_api():
+    api = Github()
+
+    yield api
